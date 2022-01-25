8a9de87d691f740e13f9869ec6cfdc3b45fe42a0
Denis Lund <vasilenko9999@gmail.com>
2022-01-25 14:17:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication19.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+class Animal
+{
+public:
+    virtual void Voice() const = 0;
+};
+
+class Cats : public Animal
+{
+public:
+   void Voice() const override
+    {
+        std::cout << "Мяяяяу\n";
+    }
+};
+
+class Wolfs : public Animal
+{
+public:
+    void Voice() const override
+    {
+        std::cout << "Ауф!\n";
+    }
+};
+
+class Snakes : public Animal
+{
+public:
+    void Voice() const override
+    {
+        std::cout << "Асссссуссссаа\n";
+    }
+};
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    Animal* animal[3];
+    animal[0] = new Cats();
+    animal[1] = new Wolfs();
+    animal[2] = new Snakes();
+
+    for (Animal* a : animal)
+        a->Voice();
+}
